4545ed69fcd016edd9a3a188acc7253a98c754bf
Eunbeann <ds5bean23@gmail.com>
2023-06-08 17:46:03 +0900

    [ refactor ] Question4 버튼, 스타일드컴포넌트 수정

--- src/@components/emotion/Question4.jsx
+++ src/@components/emotion/Question4.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import Button from "../common/button";
+import { useRecoilState } from "recoil";
+import { clickedEmotion, emotionData } from "../../recoil/emotion";
+import next_btn from "../../assets/image/next_btn.png";
+import previous_btn from "../../assets/image/previous_btn.png";
 
-export default function Question4() {
+export default function Question4({ setStep, propsName }) {
+  const [emotion, setEmotion] = useRecoilState(emotionData);
+  const [clickedMood, setClickedMood] = useRecoilState(clickedEmotion);
+
+  const [isClicked, setIsClicked] = useState(false);
+  const [id, setId] = useState(-1);
+
+  const moveToStep3 = () => {
+    setStep(3);
+  };
+
+  const moveToStep5 = () => {
+    setStep(5);
+  };
+
+  useEffect(() => {
+    console.log(emotion);
+  }, [emotion]);
+
+  const ClickedEmotionBtn = (e, id) => {
+    setIsClicked(true);
+    setId(id);
+    const newEmotions = [...emotion.emotions];
+    newEmotions.push(id);
+    setEmotion((prev) => ({ ...prev, emotions: newEmotions }));
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <St.AskBox>
+        <St.QuestionContainer>
+          <p> 알려주셔서 고마워요. </p>
+          <p> 어떤 {propsName}이었죠? </p>
+        </St.QuestionContainer>
+      </St.AskBox>
+      <St.selectContainer>
+        {clickedMood.map((item) => (
+          <St.Questions
+            isClicked={item.id === id}
+            onClick={(e) => ClickedEmotionBtn(e, item.id)}
+            key={item.id}
+            value={item.detailEmotion}>
+            {item.detailEmotion}
+          </St.Questions>
+        ))}
+      </St.selectContainer>
+      <St.ButtonContainer>
+        <St.PrevBtn onClick={moveToStep3}>이전</St.PrevBtn>
+        <St.NextBtn onClick={moveToStep5}>다음</St.NextBtn>
+      </St.ButtonContainer>
+    </>
+  );
 }
+
+const St = {
+  AskBox: styled.div`
+    background-color: rgba(256, 256, 256, 50%);
+    padding: 4rem 4rem;
+    font-size: 1.6rem;
+    margin-bottom: 7.5rem;
+  `,
+
+  QuestionContainer: styled.article`
+    display: flex;
+    flex-wrap: wrap;
+    text-align: center;
+    justify-content: center;
+    white-space: pre-line;
+  `,
+  selectContainer: styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: 50px;
+  `,
+  Questions: styled.button`
+    height: 50px;
+    width: 158px;
+    left: 0px;
+    top: 0px;
+    border-radius: 12px;
+    background: linear-gradient(120.7deg, rgba(255, 255, 255, 0.8) 5.47%, rgba(255, 255, 255, 0) 100%),
+      linear-gradient(0deg, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5));
+    margin: 5px 5px;
+    border: ${({ isClicked }) =>
+      isClicked ? "1px solid rgba(113, 123, 214, 1)" : "1px solid rgba(255, 255, 255, 1);"};
+  `,
+
+  ButtonContainer: styled.div`
+    display: flex;
+    justify-content: center;
+
+    .prev_btn {
+      height: 70;
+      width: 104px;
+      border-radius: 0px;
+    }
+    .next_btn {
+      height: 70px;
+      width: 220px;
+      border-radius: 0px;
+    }
+  `,
+};
